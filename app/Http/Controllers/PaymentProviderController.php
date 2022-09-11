edafd32177660d1a2d2c25656ebe893d7f198e33
Eng Hasan Hajjar <110938732+Eng-Hasan-Hajjar@users.noreply.github.com>
2022-09-11 09:57:21 -0700

    Update PaymentProviderController.php

--- app/Http/Controllers/PaymentProviderController.php
+++ app/Http/Controllers/PaymentProviderController.php
@@ -9,11 +9,14 @@ class PaymentProviderController extends Controller
     public function getCheckOutId(Request $request){
 
         $url = config('payment.hyperpay.url') . "/v1/checkouts";
+        
         $data = "entityId=" . config('payment.hyperpay.entity_id') .
         "&amount=". $request -> price .
         "&currency=EUR" .
         "&paymentType=DB";
 
+        
+        
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
@@ -29,9 +32,11 @@ class PaymentProviderController extends Controller
         curl_close($ch);
         $res = json_decode($responseData, true);
 
+        
         $view = view('ajax.form')->with(['responseData' => $res , 'id' => $request -> offer_id])
             ->renderSections();
 
+        
         return response()->json([
             'status' => true,
             'content' => $view['main']
