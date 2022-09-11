87eddf7c114c20c4360e43e67c1a78343bd7a829
Eng Hasan Hajjar <110938732+Eng-Hasan-Hajjar@users.noreply.github.com>
2022-09-11 10:02:05 -0700

    Update NewNotification.php

--- app/Events/NewNotification.php
+++ app/Events/NewNotification.php
@@ -4,9 +4,13 @@ namespace App\Events;
 
 use Carbon\Carbon;
 use Illuminate\Queue\SerializesModels;
+
 use Illuminate\Foundation\Events\Dispatchable;
+
 use Illuminate\Broadcasting\InteractsWithSockets;
+
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
 use Illuminate\Support\Str;
 
 class NewNotification implements ShouldBroadcast
