486a1714242f312023e92cda681eb40b0094cc5a
Eng Hasan Hajjar <110938732+Eng-Hasan-Hajjar@users.noreply.github.com>
2022-09-11 09:51:11 -0700

    Update Comment.php

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -7,16 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     protected $table = "comments";
+    
     protected $fillable=['post_id','user_id','comment','created_at','updated_at'];
+    
     protected $hidden =['created_at','updated_at'];
+    
     public $timestamps = true;
 
 
 
      //owner
     public function user(){
+    
         return $this ->  belongsTo('App\User','user_id','id');
-    }
+   
+   }
 
 
 }
