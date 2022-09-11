f65f56216e17c2c066dda55a9661214d60e5f142
Eng Hasan Hajjar <110938732+Eng-Hasan-Hajjar@users.noreply.github.com>
2022-09-11 09:54:18 -0700

    Update Order.php

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,8 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     protected $table = "comments";
+    
     protected $fillable=['order_no','total_amount','user_id','offer_id','bank_transaction_id','created_at','updated_at'];
+    
     protected $hidden =['created_at','updated_at'];
+    
     public $timestamps = true;
 
 
