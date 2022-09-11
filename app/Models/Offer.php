7a781d959f92ddfe974b293ebfb5adcbb5084465
Eng Hasan Hajjar <110938732+Eng-Hasan-Hajjar@users.noreply.github.com>
2022-09-11 09:53:35 -0700

    Update Offer.php

--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -7,8 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Offer extends Model
 {
     protected $table = "offers";
+    
     protected $fillable=['title','description','price','created_at','updated_at'];
+    
     protected $hidden =['created_at','updated_at'];
+    
     public $timestamps = true;
 
 }
