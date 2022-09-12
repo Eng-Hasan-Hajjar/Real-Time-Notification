48ee334ea26d0709f62f5ad352e4231bff173b99
Eng Hasan Hajjar <110938732+Eng-Hasan-Hajjar@users.noreply.github.com>
2022-09-12 12:47:51 -0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -40,6 +40,7 @@ require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
+
 /*
 |--------------------------------------------------------------------------
 | Run The Application
@@ -57,8 +58,10 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
+    
     $request = Illuminate\Http\Request::capture()
 );
+
 
 $response->send();
 
